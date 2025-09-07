d7c767b6a27b1f6a596064ae362c859e5a5dd529
Chase Kusterer <chasekusterer@MacBookPro.attlocal.net>
2025-09-07 16:42:20 -0700

    Commit all local changes

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -1,3 +1,13 @@
 # Force classic Notebook homepage
 c = get_config()
 c.NotebookApp.default_url = '/tree'
+
+# hiding some directory items when the book is opened
+c.ContentsManager.hide_globs = [
+    '__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib', '*.yml'
+    '**/.ipynb_checkpoints',
+    'postBuild', # postBuild file
+    'README.md', # GitHub repo README file
+    'images',    # images folder
+    'documents', # documents folder
+]
