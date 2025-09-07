1930c11e4c25914fdd17059ccc7942d9026b9d25
Chase Kusterer <50032310+chase-kusterer@users.noreply.github.com>
2025-09-07 16:56:16 -0700

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -4,10 +4,11 @@ c.NotebookApp.default_url = '/tree'
 
 # hiding some directory items when the book is opened
 c.ContentsManager.hide_globs = [
-    '__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib', '*.yml'
+    '__pycache__', '*.pyc', '*.pyo', '.DS_Store', '*.so', '*.dylib',
     '**/.ipynb_checkpoints',
-    'postBuild', # postBuild file
-    'README.md', # GitHub repo README file
-    'images',    # images folder
-    'documents', # documents folder
+    'postBuild',       # postBuild file
+    'README.md',       # GitHub repo README file
+    'images',          # images folder
+    'documents',       # documents folder
+    'environment.yml', # environment file
 ]
